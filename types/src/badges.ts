42c9f1b28c49e454f472810b62257430ad4353b3
Henry | 0xhank <71292860+0xhank@users.noreply.github.com>
2022-08-29 21:01:35 -0400

    feat: new badge designs (#37)

--- types/src/badges.ts
+++ types/src/badges.ts
@@ -1,15 +1,16 @@
-import { Abstract } from "./utility";
+import { Abstract } from './utility';
 
 export type BadgeType = Abstract<string, 'BadgeType'>;
 
 export const BadgeType = {
-  Dfdao: 'Dfdao' as BadgeType,
   StartYourEngine: 'StartYourEngine' as BadgeType,
-  Wallbreaker: 'Wallbraeker' as BadgeType
+  Wallbreaker: 'Wallbreaker' as BadgeType,
+  Tree: 'Tree' as BadgeType,
+  Nice: 'Nice' as BadgeType,
+  Sleepy: 'Sleepy' as BadgeType,
 } as const;
 
 export type GrandPrixBadge = {
   configHash: string;
   badge: BadgeType;
-} 
-
+};
